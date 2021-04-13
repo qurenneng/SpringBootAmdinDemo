87ff9201f2dfc25bf382d9aff673de76cb1fc6f3
cainiao <1796789910@qq.com>
2021-04-13 17:36:26 +0800

    Zookeeper 模块 登录记住我功能配置

--- SpringBootAdminZookeeper/src/main/java/com/example/springbootadminsamplezookeeper/SpringBootAdminSampleZookeeperApplication.java
+++ SpringBootAdminZookeeper/src/main/java/com/example/springbootadminsamplezookeeper/SpringBootAdminSampleZookeeperApplication.java
@@ -4,11 +4,13 @@ import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -55,8 +57,11 @@ public class SpringBootAdminSampleZookeeperApplication {
 
         private final AdminServerProperties adminServer;
 
-        public SecuritySecureConfig(AdminServerProperties adminServer) {
+        private final SecurityProperties security;
+
+        public SecuritySecureConfig(AdminServerProperties adminServer,SecurityProperties security) {
             this.adminServer = adminServer;
+            this.security = security;
         }
 
         @Override
@@ -79,6 +84,17 @@ public class SpringBootAdminSampleZookeeperApplication {
                                     new AntPathRequestMatcher(this.adminServer.path("/instances/*"),
                                             HttpMethod.DELETE.toString()),
                                     new AntPathRequestMatcher(this.adminServer.path("/actuator/**"))));
+        }
+
+        /**
+         * 记住我功能配置，当点击记住我时候，必须有以下配置，否则无法登录成功。
+         * @param auth
+         * @throws Exception
+         */
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication().withUser(security.getUser().getName())
+                    .password("{noop}" + security.getUser().getPassword()).roles("USER");
         }
 
     }
