80ac21fe68d74f25b3a809abf98f94c254db7cc9
cainiao <1796789910@qq.com>
2021-04-13 16:04:54 +0800

    增加Consul 做为注册中心整合SpringBootAdmin

---
+++ SpringBootAdminConsulService/src/test/java/com/example/adminconsul/AdminconsulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adminconsul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminconsulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
